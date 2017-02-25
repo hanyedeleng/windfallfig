014894cb19ceb1d8f970e7f2b85b920b8b08e04e
hanyedeleng <hanyedeleng@gmail.com>
2017-02-25 10:56:02 -0500

    all pyc files removed

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Movie
+class Movie(models.Model):
+    # name = movie name + year, make it unique
+    name = models.CharField(max_length=128, unique=True) 
+
+    # poster link is the path to the poster(image) file
+    posterLink = models.CharField(max_length=128)
+
+    # basic information of a movie, e.g. director, writer, stars
+    basicInfo = models.CharField(max_length=256)
+
+    # synopsis
+    synopsis = models.CharField(max_length=512)
+    relatedLinks = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
--- movies/views.py
+++ movies/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+# main page
 def index(request):
     return render(request,'movies/index.html')
 
+# about page
 def about(request):
     return render(request,'movies/about.html')
